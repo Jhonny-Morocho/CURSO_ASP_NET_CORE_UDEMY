251d7c5c36b04ce2f4ee8c1fb2f7ca5984b4aa5e
Jhonny-Morocho <37280267+Jhonny-Morocho@users.noreply.github.com>
2022-09-19 01:53:12 -0500

    video 82

--- Backend/Startup.cs
+++ Backend/Startup.cs
@@ -1,6 +1,7 @@
 using Backend.Repositorios;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -10,6 +11,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -43,15 +45,53 @@ namespace Backend
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        //
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,ILogger<Startup> logger)
         {
+            //solo para probar
+            //app.Run(async context=> {
+            //    await context.Response.WriteAsync("Estoy interceptando el pipiline");
+            //});
+
+            //ejecuta esta linena y sigue la siguiente no la para
+            app.Use(async (contex,next)=> {
+                using (var swapStream=new MemoryStream())
+                {
+                    var respuestaOriginal = contex.Response.Body;
+                    contex.Response.Body = swapStream;
+                    await next.Invoke();
+
+                    swapStream.Seek(0, SeekOrigin.Begin);
+                    string respuesta = new StreamReader(swapStream).ReadToEnd();
+                    swapStream.Seek(0,SeekOrigin.Begin);
+
+                    await swapStream.CopyToAsync(respuestaOriginal);
+                    contex.Response.Body = respuestaOriginal;
+
+
+                    logger.LogInformation(respuesta);
+
+
+                }
+            });
+
+            app.Map("/map1",(app)=> {
+                app.Run(async context=>
+                {
+                    await context.Response.WriteAsync("Estoy incertando el pipeline"); 
+                });
+            });
+
+
             if (env.IsDevelopment())
             {
+                //definiendo milleword
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Backend v1"));
             }
-
+            //tambien es un middleware
+            // los q empiezan con use no detiene el proceso
             app.UseHttpsRedirection();
 
             app.UseRouting();
